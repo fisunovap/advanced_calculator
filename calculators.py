a9f7a5293c52e8b3fe43c114250e0bba0799cbb7
fisunoff <fisunoff@yandex.ru>
2022-12-31 17:23:14 +0300

    Сделал перевод в системах счисления; рефакторинг кода

--- calculators.py
+++ calculators.py
@@ -6,14 +6,14 @@ def solver_x4(a, b, c):
     D = b * b - 4 * a * c
     if D >= 0:
         x1 = (-b + sqrt(D)) / (2 * a)
-        if x1>=0:
+        if x1 >= 0:
             x_1 = sqrt(x1)
-            x_2 = (0-sqrt(x1))
+            x_2 = (0 - sqrt(x1))
         else:
             x_1 = "sqrt(t1)<0"
             x_2 = "sqrt(t1)<0"
         x2 = (-b - sqrt(D)) / (2 * a)
-        if x2>=0:
+        if x2 >= 0:
             x_3 = sqrt(x2)
             x_4 = (0 - sqrt(x2))
         else:
@@ -23,3 +23,36 @@ def solver_x4(a, b, c):
     else:
         text = "Дискриминант = %s \nНет корней в поле действительных чисел" % D
     return text
+
+
+def solver_x2(a, b, c):
+    """ Решает квадратное уравнение и возвращает результат в форматированной строке """
+    D = b * b - 4 * a * c
+    if D >= 0:
+        x1 = (-b + sqrt(D)) / (2 * a)
+        x2 = (-b - sqrt(D)) / (2 * a)
+        text = "Дискриминант = %s \nX1 = %s \nX2 = %s \n" % (D, x1, x2)
+    else:
+        text = "Дискриминант = %s \nНет корней в поле действительных чисел" % D
+    return text
+
+
+def to_new_system(n: str, old_system: int, new_system: int) -> str or bool:
+    """
+    Перевод в систему счисления с основанием от 2 до 35
+    :param n: Число для перевода
+    :param old_system: Старая система счисления
+    :param new_system: Новая система счисления
+    :return: Число в новой системе счисления
+    """
+    if new_system >= 36:
+        raise ValueError
+    n_dict = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+              'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    int_10 = int(n, old_system)
+    result = ""
+    while int_10 > 0:
+        result = n_dict[int_10 % new_system] + result
+        int_10 //= new_system
+
+    return result
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from tkinter import *
 from calculators import *
 
+
 class Main(Frame):
     def __init__(self, root):
         super(Main, self).__init__(root)
+        self.formula = "0"
+        self.lbl = Label(text=self.formula, font=("Times New Roman", 21, "bold"), bg="#000", foreground="#FFF")
         self.build()
 
     def build(self):
-        self.formula = "0"
-        self.lbl = Label(text=self.formula, font=("Times New Roman", 21, "bold"), bg="#000", foreground="#FFF")
         self.lbl.place(x=11, y=50)
 
         btns = [
@@ -33,146 +34,158 @@ class Main(Frame):
                 x = 10
                 y += 81
 
+    def inserter(self, value, output):
+        """ Вставляет указанное значение в текстовый виджет """
+        output.delete("0.0", "end")
+        output.insert("0.0", value)
 
+    def clear(self, event):
+        """ Очищает форму ввода """
+        caller = event.widget
+        caller.delete("0", "end")
 
     def logicalc(self, operation):
-        if operation == "C":
-            self.formula = ""
-        elif operation == "DEL":
-            self.formula = self.formula[0:-1]
-        elif operation == "X^2":
-            self.formula += "^2"
-        elif operation == "=":
-            self.formula = str(eval(self.formula.replace("^2", "**2")))
-        elif operation == "ax^2+bx+c":
-            from math import sqrt
-            def solver(a, b, c):
-                """ Решает квадратное уравнение и возвращает результат в форматированной строке """
-                D = b * b - 4 * a * c
-                if D >= 0:
-                    x1 = (-b + sqrt(D)) / (2 * a)
-                    x2 = (-b - sqrt(D)) / (2 * a)
-                    text = "Дискриминант = %s \nX1 = %s \nX2 = %s \n" % (D, x1, x2)
-                else:
-                    text = "Дискриминант = %s \nНет корней в поле действительных чисел" % D
-                return text
-
-
-
-            root = Tk()
-            root.title("ax^2+bx+c")
-            root.minsize(325, 230)
-            root.resizable(width=False, height=False)
-
-            frame = Frame(root)
-            frame.grid()
-
-            a = Entry(frame, width=3)
-            a.grid(row=1, column=1, padx=(10, 0))
-            a.bind("<FocusIn>", clear)
-            a_lab = Label(frame, text="x^2+").grid(row=1, column=2)
-
-            b = Entry(frame, width=3)
-            b.bind("<FocusIn>", clear)
-            b.grid(row=1, column=3)
-            b_lab = Label(frame, text="x+").grid(row=1, column=4)
-
-            c = Entry(frame, width=3)
-            c.bind("<FocusIn>", clear)
-            c.grid(row=1, column=5)
-            c_lab = Label(frame, text="= 0").grid(row=1, column=6)
-
-            but = Button(frame, text="Решить", command=handler).grid(row=1, column=7, padx=(10, 0))
-
-            output = Text(frame, bg="black", font="Arial 12", fg="white", width=35, height=10)
-            output.grid(row=2, columnspan=8)
-
-            root.mainloop()
-
-        elif operation == "ax^4+bx^2+c":
-
-
-
-            def inserter(value):
-                """ Вставляет указанное значение в текстовый виджет """
-                output.delete("0.0", "end")
-                output.insert("0.0", value)
-
-            def clear(event):
-                """ Очищает форму ввода """
-                caller = event.widget
-                caller.delete("0", "end")
-
-            def handler():
-                """ Получает содержимое записей и передает результат в текст """
-                try:
-                    # проверка, что мы ввели правильные значения
-                    a_val = float(a.get())
-                    b_val = float(b.get())
-                    c_val = float(c.get())
-                    inserter(solver_x4(a_val, b_val, c_val))
-                except ValueError:
-                    inserter("Убедитесь, что вы ввели 3 цифры")
-
-            root = Tk()
-            root.title("ax^4+bx^2+c")
-            root.minsize(325, 230)
-            root.resizable(width=False, height=False)
-
-            frame = Frame(root)
-            frame.grid()
-
-            a = Entry(frame, width=3)
-            a.grid(row=1, column=1, padx=(10, 0))
-            a.bind("<FocusIn>", clear)
-            a_lab = Label(frame, text="x^4+").grid(row=1, column=2)
-
-            b = Entry(frame, width=3)
-            b.bind("<FocusIn>", clear)
-            b.grid(row=1, column=3)
-            b_lab = Label(frame, text="x^2+").grid(row=1, column=4)
-
-            c = Entry(frame, width=3)
-            c.bind("<FocusIn>", clear)
-            c.grid(row=1, column=5)
-            c_lab = Label(frame, text="= 0").grid(row=1, column=6)
-
-            but = Button(frame, text="Решить", command=handler).grid(row=1, column=7, padx=(10, 0))
-
-            output = Text(frame, bg="black", font="Arial 12", fg="white", width=35, height=10)
-            output.grid(row=2, columnspan=8)
-
-            root.mainloop()
-        elif operation == "СС":
-            root = Tk()
-            root.title('СС')
-
-            buttons = (('2', '3', '5', '8', '10'),
-                       ('12', '16', '20', '60'),
-                       ('CC input', 'СС output'))
-
-            activeStr = ''
-            stack = []
-            root.minsize(200, 120)
-            root.resizable(width=False, height=False)
-
-
-            button = Button(root, text='CE', command=lambda text='CE': click(text))
-            button.grid(row=1, column=3, sticky="nsew")
-            for row in range(4):
-                for col in range(4):
-                    button = Button(root, text=buttons[row][col],
-                                    command=lambda row=row, col=col: click(buttons[row][col]))
-                    button.grid(row=row + 2, column=col, sticky="nsew")
-
-            root.mainloop()
-
-        else:
-            if self.formula == "0":
+        match operation:
+            case "C":
                 self.formula = ""
-            self.formula += operation
-        self.update()
+            case "DEL":
+                self.formula = self.formula[0:-1]
+            case "X^2":
+                self.formula += "^2"
+            case "=":
+                self.formula = str(eval(self.formula.replace("^2", "**2")))
+            case "ax^2+bx+c":
+                def handler():
+                    """ Получает содержимое записей и передает результат в текст """
+                    try:
+                        # проверка, что мы ввели правильные значения
+                        a_val = float(a.get())
+                        b_val = float(b.get())
+                        c_val = float(c.get())
+                        self.inserter(solver_x2(a_val, b_val, c_val), output)
+                    except ValueError:
+                        self.inserter("Убедитесь, что вы ввели 3 цифры", output)
 
+                root = Tk()
+                root.title("ax^2+bx+c")
+                root.minsize(325, 230)
+                root.resizable(width=False, height=False)
+
+                frame = Frame(root)
+                frame.grid()
+
+                a = Entry(frame, width=3)
+                a.grid(row=1, column=1, padx=(10, 0))
+                a.bind("<FocusIn>", self.clear)
+                Label(frame, text="x^2+").grid(row=1, column=2)
+
+                b = Entry(frame, width=3)
+                b.bind("<FocusIn>", self.clear)
+                b.grid(row=1, column=3)
+                Label(frame, text="x+").grid(row=1, column=4)
+
+                c = Entry(frame, width=3)
+                c.bind("<FocusIn>", self.clear)
+                c.grid(row=1, column=5)
+                Label(frame, text="= 0").grid(row=1, column=6)
+
+                Button(frame, text="Решить", command=handler).grid(row=1, column=7, padx=(10, 0))
+
+                output = Text(frame, bg="black", font="Arial 12", fg="white", width=35, height=10)
+                output.grid(row=2, columnspan=8)
+
+                root.mainloop()
+
+            case "ax^4+bx^2+c":
+                def handler():
+                    """ Получает содержимое записей и передает результат в текст """
+                    try:
+                        # проверка, что мы ввели правильные значения
+                        a_val = float(a.get())
+                        b_val = float(b.get())
+                        c_val = float(c.get())
+                        self.inserter(solver_x4(a_val, b_val, c_val), output)
+                    except ValueError:
+                        self.inserter("Убедитесь, что вы ввели 3 цифры", output)
+
+                root = Tk()
+                root.title("ax^4+bx^2+c")
+                root.minsize(325, 230)
+                root.resizable(width=False, height=False)
+
+                frame = Frame(root)
+                frame.grid()
+
+                a = Entry(frame, width=3)
+                a.grid(row=1, column=1, padx=(10, 0))
+                a.bind("<FocusIn>", self.clear)
+                Label(frame, text="x^4+").grid(row=1, column=2)
+
+                b = Entry(frame, width=3)
+                b.bind("<FocusIn>", self.clear)
+                b.grid(row=1, column=3)
+                Label(frame, text="x^2+").grid(row=1, column=4)
+
+                c = Entry(frame, width=3)
+                c.bind("<FocusIn>", self.clear)
+                c.grid(row=1, column=5)
+                Label(frame, text="= 0").grid(row=1, column=6)
+
+                Button(frame, text="Решить", command=handler).grid(row=1, column=7, padx=(10, 0))
+
+                output = Text(frame, bg="black", font="Arial 12", fg="white", width=35, height=10)
+                output.grid(row=2, columnspan=8)
+
+                root.mainloop()
+            case "СС":
+                def handler():
+                    """ Получает содержимое записей и передает результат в текст """
+                    try:
+                        # проверка, что мы ввели правильные значения
+                        a_val = str(a.get())
+                        s_val = int(b.get())
+                        f_val = int(c.get())
+                        result = to_new_system(a_val, s_val, f_val)
+                        self.inserter(result, output)
+                    except ValueError:
+                        self.inserter("Убедитесь, что введены верные значения", output)
+
+
+                root = Tk()
+                root.title("СС")
+                root.minsize(450, 100)
+                root.resizable(width=False, height=False)
+
+                frame = Frame(root)
+                frame.grid()
+
+                a = Entry(frame, width=20)
+                a.grid(row=1, column=1, padx=(10, 0))
+                a.bind("<FocusIn>", self.clear)
+                Label(frame, text="Число").grid(row=1, column=2)
+
+                b = Entry(frame, width=3)
+                b.bind("<FocusIn>", self.clear)
+                b.grid(row=1, column=3)
+                Label(frame, text="Исходная СС").grid(row=1, column=4)
+
+                c = Entry(frame, width=3)
+                c.bind("<FocusIn>", self.clear)
+                c.grid(row=1, column=5)
+                Label(frame, text="Конечная СС").grid(row=1, column=6)
+
+                Button(frame, text="Решить", command=handler).grid(row=1, column=7, padx=(10, 0))
+
+                output = Text(frame, bg="black", font="Arial 12", fg="white", width=50, height=10)
+                output.grid(row=2, columnspan=8)
+
+                root.mainloop()
+
+            case _:
+                if self.formula == "0":
+                    self.formula = ""
+                self.formula += operation
+                self.update()
 
     def update(self):
         if self.formula == "":
